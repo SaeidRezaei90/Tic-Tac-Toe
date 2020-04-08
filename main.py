a000f2ce4aaf5e5fc351989896e1c766f7ccad58
Saeid <saeid69.rezaei@gmail.com>
2020-04-08 12:35:09 +0000

    Last version, fixed some bugs

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #  Steps that should be implemented:
 # 1 create a 3*3 Board
 # 2 display Board
-# 3 Play Game
+# 3 start Playing Game
 # 4 handle turn
-# 5 Check Win
-#    Check row
-#    Check column
-#    Check diagonals
-# 6 Check tie
-# 7 Flip player
+# 5 Check Game is ended
+#   5.1 Check Win
+#       Check row
+#       Check column
+#       Check diagonals
+#   5.2 Check tie
+# 6 Flip player
 
 #### hadle that a player can not replace in another one
 
@@ -18,7 +19,6 @@ from enum import Enum
 
 # A 3*3 Board
 board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
-
 
 # define an enum to determine current player
 class player(Enum):
@@ -34,7 +34,6 @@ game_still_going = True
 # who won? or tie
 winner = None
 
-
 # Display the initial stage of board
 def display_board():
     print(board[0] + ' | ' + board[1] + ' | ' + board[2])
@@ -45,24 +44,27 @@ def display_board():
 #  Start the game
 def play_game():
   
-  #declare global variable
+  # declare global variable
   global current_player
 
-  #ask which player start the game (X or O)
+  # ask which player start the game (X or O)
   initial_player = input("Which player wants to start the game, X or O ? ")
-  if (str(initial_player).lower() == 'x'):
-    current_player = player.X_player
-  elif (str(initial_player).lower() == 'o'):
-    current_player = player.O_player
-  else:
-    initial_player = input("Please select between O and X only: ")
+  valid_initial_player = True
+  while valid_initial_player:
+    if (str(initial_player).lower() == 'x'):
+      current_player = player.X_player
+      valid_initial_player = False
+    elif (str(initial_player).lower() == 'o'):
+      current_player = player.O_player
+      valid_initial_player = False
 
-  
+    else:
+      initial_player = input("Please select a character between O and X only: ")
 
-  #  first we should display the board
+    # first we should display the board
   display_board()
 
-  #  continue the game since game_still_going be false
+  # continue the game since game_still_going be false
   while game_still_going:
       # handle the movement of player
       handle_turn(current_player)
@@ -76,9 +78,9 @@ def play_game():
   # The game has ended
   print()
   if winner == player.X_player:
-      print('X won, Congratulatuon')
+      print('X won, congratulation')
   elif winner == player.O_player:
-      print('O won, Congratulatuon')
+      print('O won, congratulation')
   else:
       print('Tie.')
 
@@ -91,16 +93,25 @@ def handle_turn(player):
     while is_not_digit:
         if (position.isdigit() and int(position) < 10 and int(position) > 0):
             position = int(position) - 1
-            is_not_digit = False
         else:
             position = input("please enter a valid number between 1-9: ")
+            continue
+        
+        if(board[position] == '-'):
+          # Check 'X' should be written or 'O'
+          if(player == player.X_player):
+            board[position] = "X"
+          else:
+            board[position] = "O"
 
-    # Check 'X' should be written or 'O'
-    if(player == player.X_player):
-      board[position] = "X"
-    else:
-      board[position] = "O"
+          is_not_digit = False
+        
+        else:
+         print()
+         position = input("please enter a valid number between 1-9, note that the entered position should be empty '-'.")
 
+    
+    
     display_board()
 
 
